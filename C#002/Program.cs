3a4eb3a8a0e9d15a80b100afa0d243326c6fa8ac
Sofia <lucifersatandf@gmail.com>
2023-07-14 17:38:11 +0300

    .

---
+++ C#002/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+void Main()
+{
+    int M = ReadInt("Введите начальное число M: ");
+    int N = ReadInt("Введите конечное число N: ");
+    if (M < 0 || N < 0)
+    {
+        System.Console.WriteLine("Вы ввели некорректные числа.");
+        return;
+    }
+    System.Console.WriteLine($"Число, определённое функцией Аккермана по введённым параметрам равно: {FuncAckerman(M, N)}");
+}
+
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int FuncAckerman(int M, int N)
+{
+    if (M == 0) return N + 1;
+    else if (N == 0) return FuncAckerman(M - 1, 1);
+    else
+    {
+        return FuncAckerman(M - 1, FuncAckerman(M, N - 1));
+    }
+}
+
+
+Main();
---
+++ С#001/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+void Main()
+{
+    int N = ReadInt("Введите начальное число M: ");
+    int M = ReadInt("Введите конечное число N: ");
+
+    if (M < N)
+    {
+        SumNumbers(M, N);
+        System.Console.WriteLine($"Сумма всех натуральных чисел в заданном диапазоне равна: {SumNumbers(M, N)}");
+    }
+    else
+    {
+        SumNumbers(N, M);
+        System.Console.WriteLine($"Сумма всех натуральных чисел в заданном диапазоне равна: {SumNumbers(N, M)}");
+    }
+}
+
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int SumNumbers(int M, int N)
+{
+    if (M == N) return M;
+    return N + SumNumbers(M, N - 1);
+}
+
+
+Main();
